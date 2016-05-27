3d6621a6621c155e68e11c9edec47be4881d966e
sagnikb <sagnik6696@gmail.com>
2016-05-27 21:48:43 +0530

    Skeleton code for main body

--- Gravity2.c
+++ Gravity2.c
@@ -4,37 +4,63 @@
 #define size 10000
 #define timeInc 0.01
 
-float field[size][size][size];
+//float field[size][size][size];
 
 typedef struct vector{
 	float x;
 	float y;
 	float z;
-} v;
+} vec;
 
-typedef struct planet{
+typedef struct planets{
 	float mass;
-	v pos;
-	v vel;
-	//v acc;	
-} p;
+	vec pos;
+	vec vel;	
+} planet;
 
-v input(p planet){
-	printf("m,x,y,z,vx,vy,vz in order:");
-	scanf("%f,%f,%f,%f,%f,%f,%f",&p.mass,&p.pos.x,
+//typedef struct planets planet;
 
-
-p init_planet(float mass, v position, v velocity){
-	//malloc 
-	p.vel = velocity;
-	p.pos = position;
+planet * initPlanet(){
+	planet * p = (planet*)malloc(sizeof(planet));
+	printf("NEW PLANET!\n");
+	printf("Specify initial position:");
+	scanf("%f%f%f", &p->pos.x,&p->pos.y,&p->pos.z);
+	printf("\n");
+	printf("Specify initial velocity:");
+	scanf("%f%f%f", &p->vel.x,&p->vel.y,&p->vel.z);
+	printf("\n");
+	printf("Specify planet mass:");
+	scanf("%f", &p->mass);
+	printf("\n");
 	return p;
 }
 
+void printPlanet(planet p){
+	printf("Mass = %f\n", p.mass);
+	printf("Position = (%f, %f, %f)\n", p.pos.x, p.pos.y, p.pos.z);
+	printf("Velocity = (%f, %f, %f)\n", p.vel.x, p.vel.y, p.vel.z);
+	int i = 0;
+	for(i = 0; i < 42; i++){
+		printf("-");
+	}
+	printf("\n");
+	return;
+}
+
+
 int main(){
-	p p1 = init_planet();
-	p p2 = init_planet();
-	
-	
+	int n;
+	printf("Please enter number of required planets:");
+	scanf("%d", &n);
+	printf("\n");
+	planet** planetArray = (planet**)malloc(n*sizeof(planet *));
+	int i;
+	for(i = 0; i < n; i++){
+		planetArray[i] = initPlanet();
+	}
+	for(i = 0; i < n; i++){
+		printf("Planet No: %d\n", i+1);
+		printPlanet(*planetArray[i]);
+	}	
 	return 0;
 }
